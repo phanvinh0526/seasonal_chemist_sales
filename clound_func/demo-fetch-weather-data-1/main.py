1a8167aa73780ea061f03937f7624c15fc3f7df7
vinh phan <phanvinh0526@vinhsMacStudio.gateway>
2024-06-27 15:45:53 +1000

    update func

--- clound_func/demo-fetch-weather-data-1/main.py
+++ clound_func/demo-fetch-weather-data-1/main.py
@@ -1,6 +1,7 @@
 import functions_framework
 import json
 import requests
+from datetime import datetime as dt
 import time, datetime, calendar
 from google.cloud import storage
 from google.cloud.sql.connector import Connector, IPTypes
@@ -10,17 +11,20 @@ import sqlalchemy, pymysql
 APP_ID = 'd16b1167069c24328f77797175f4d365'
 LOCATION = 'Melbourne,AU'
 WEATHER_DATA_URL = f'http://api.openweathermap.org/data/2.5/weather?q={LOCATION},de&units=metric&APPID={APP_ID}'
-FILE_NAME = f'weather_current_date_{calendar.timegm(datetime.datetime.now().timetuple())}.csv'
+FILE_NAME = f'weather_current_date_{dt.today().strftime("%Y%m%d")}.csv'
+
+BUCKET_NAME = "stg-demo-cw-retailweather-da"
+TAR_STORAGE_PATH = f"{BUCKET_NAME}/weatherdata/{dt.today().strftime('%Y%m%d')}.csv" # weather_data_20240627
 
 # Cloud SQL Connection
-connection_name = "amiable-vent-425413-a7:us-central1:demo-seasonal-sales-analysis"
-public_ip = "34.28.50.62"
-port = 3306
-db_name = "demo-seasonal-sales-analysis"
-db_user = "admin"
-db_password = "admin1234"
-driver_name = 'mysql+pymysql'
-query_string = dict({"unix_socket": "/cloudsql/{}".format(connection_name)})
+# connection_name = "amiable-vent-425413-a7:us-central1:demo-seasonal-sales-analysis"
+# public_ip = "34.28.50.62"
+# port = 3306
+# db_name = "demo-seasonal-sales-analysis"
+# db_user = "admin"
+# db_password = "admin1234"
+# driver_name = 'mysql+pymysql'
+# query_string = dict({"unix_socket": "/cloudsql/{}".format(connection_name)})
 
 # TEST API CALL
 # http://api.openweathermap.org/data/2.5/weather?q=Melbourne,AU,de&units=metric&APPID=d16b1167069c24328f77797175f4d365
@@ -31,52 +35,45 @@ def get_data():
     response = requests.get(WEATHER_DATA_URL)
     data = json.loads(response.text)
 
+    dt = data['dt']
     temp = data['main']['temp']
     pressure = data['main']['pressure']
     temp_min = data['main']['temp_min']
     temp_max = data['main']['temp_max']
     humidity = data['main']['humidity']
     wind_speed = data['wind']['speed']
-    try:
-        wind_gust = data['wind']['gust']
-    except KeyError:
-        wind_gust = None
-    wind_deg = data['wind']['deg']
-    clouds = data['clouds']['all']
-    try:
-        rain = data['rain']['3h']
-    except KeyError:
-        rain = None
-    try:
-        snow = data['snow']['3h']
-    except KeyError:
-        snow = None
-    weather_id = data['weather'][0]['id']
-    sunrise = data['sys']['sunrise']
-    sunset = data['sys']['sunset']
-
-    return [temp, pressure, temp_min, temp_max, humidity, wind_speed, wind_gust, wind_deg, clouds, rain, snow,
-            weather_id, sunrise, sunset]
-
-
+    weather_desc = data['weather'][0]['description']
+    # res = {
+    #     "dt": dt,
+    #     "temp": temp,
+    #     "humidity": humidity,
+    #     "wind_speed": wind_speed,
+    #     "condition": weather_desc
+    # }
+    res = [dt, temp, humidity, wind_speed, weather_desc]
+    print(res)
+    return res
 
 def save_data_to_bucket(data):
-    blob = BUCKET.blob(FILE_NAME)
-    blob.upload_from_string(data=json.dumps(data), content_type='application/json')
-    return 'JSON file (FILE_NAME) successfully uploaded to ' + bucket_name
+    # blob = BUCKET.blob(FILE_NAME)
+    # blob.upload_from_string(data=json.dumps(data), content_type='application/json')
+    # return 'JSON file (FILE_NAME) successfully uploaded to ' + FILE_NAME
+    client = storage.Client()
+    bucket = client.get_bucket(BUCKET_NAME)
+    blob = bucket.blob(TAR_STORAGE_PATH)
+    with blob.open(mode='w') as f:
+        f.write("hello world!!!")
+        # for line in data:
+        #     f.write(line)
 
-
-# def save_data_to_db(data):
-#     con = sqlite3.connect("PATH_TO_SQLITE_FILE")
-#     con.isolation_level = None
-#     cur = con.cursor()
-#     query = '''INSERT INTO open_weather_data VALUES (null, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'''
-#     cur.execute(query, tuple([datetime.datetime.now().isoformat()] + data[:-2]))
-#     con.commit()
-#     con.close()
-
-# initialize Cloud SQL Connector
-connector = Connector()
+def save_data_to_db(data):
+    con = sqlite3.connect("PATH_TO_SQLITE_FILE")
+    con.isolation_level = None
+    cur = con.cursor()
+    query = '''INSERT INTO open_weather_data VALUES (null, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'''
+    cur.execute(query, tuple([datetime.datetime.now().isoformat()] + data[:-2]))
+    con.commit()
+    con.close()
 
 # SQLAlchemy database connection creator function
 def getconn():
@@ -89,25 +86,6 @@ def getconn():
         ip_type=IPTypes.PUBLIC # IPTypes.PRIVATE for private IP
     )
     return conn
-
-# create SQLAlchemy connection pool
-pool = sqlalchemy.create_engine(
-    "mysql+pymysql://",
-    creator=getconn,
-)
-
-# interact with Cloud SQL database using connection pool
-with pool.connect() as db_conn:
-    # query database
-    result = db_conn.execute("SELECT * from products_by_date").fetchall()
-    print(result)
-    # Do something with the results
-    for row in result:
-        print(row)
-
-# close Cloud SQL Connector
-connector.close()
-
 
 def save_data_to_db(request):   #You can change this to match your personal details
     stmt = sqlalchemy.text('''INSERT INTO open_weather_data VALUES (null, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''')
@@ -150,12 +128,11 @@ def fetch_weather_data_api(request):
         Response object using `make_response`
         <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
     """
+    print("Function started...")
     data = get_data()
-    time.sleep(10)
-    now = calendar.timegm(datetime.datetime.now().timetuple())
-    if data[-2] < now < data[-1]:
-        save_data_to_db(data)
+    time.sleep(2)
+    save_data_to_bucket(data)
 
 
 # Involke
-# functions-framework --target fetch_weather_data_api
+# functions-framework --target fetch_weather_data_api --debug
